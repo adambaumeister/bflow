5287fe616bb7b71cad419c1abec407bca68628e9
adam <abigserve@gmail.com>
2017-10-12 21:33:29 +1100

    Getting further with l2 path calc

--- libtest.py
+++ libtest.py
@@ -1,30 +1,6 @@
 #!/usr/bin/python
-from lib.topology import Path,Link
-"""
-"""
-nodes = {
-    'n1' : ['n2','n3'], 
-    'n2' : ['n1','n3'],
-    'n3' : ['n2','n1','n4','n5'],
-    'n4' : ['n3'],
-    'n5' : ['n3'],
-}
-"""
-    n1===n2" 
-    test
-""" 
-nodes2 = {
-    'n1' : ['n2','n2'] 
-}
-l1 = Link('n1','n2',1)  
-l2 = Link('n2','n1',2)
-l3 = Link('n2','n3',1) 
-#l4 = Link('n3','n2',2) 
-#links = [l1,l2,l3,l4]  
-links = [l1,l3]  
-#for k,v in nodes.items(): 
-#    print "{0} {1}".format(k,v) 
-p = Path()
-p.add_link(l1)
-p.add_link(l2)
-p.spf_links('n1','n3') 
+from lib.topology import topology
+
+t = topology
+t.add_link(1,2,1)
+t.add_link(2,1,1)
