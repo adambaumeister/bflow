941e2a1bf280b3b0f6ef293faceda9d8251869d6
adam <abigserve@gmail.com>
2017-10-12 21:45:30 +1100

    Getting further with l2 path calc

--- lib/topology.py
+++ lib/topology.py
@@ -103,7 +103,7 @@ class topology:
                 if id != id2:
                     try:
                         link = self.path.next_hop(str(id), str(id2))
-                        print "id {0} to id {1} next hop port = {2}".format(id, id2, link.ports[id])
+                        print "id {0} to id {1} next hop port".format(id, id2)
                     except KeyError as e:
                         print "No path between {0} and {1}!".format(id,id2)
                         print e.message
