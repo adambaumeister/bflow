8a8e20ab125bd43773d897abbcddbe2f58cf8039
adam <abigserve@gmail.com>
2017-10-15 11:30:49 +1100

    Started on broadcast forawrding

--- libtest.py
+++ libtest.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python
-from lib.topology import topology
+from lib.topology import topology, Link, Path
 
 t = topology("spaghett")
-n1 = str(1)
-n2 = str(2)
-n3 = str(3)
-n4 = str(4)
-t.add_link(n1, n2, 1)
-t.add_link(n2, n3, 2)
-t.add_link(n4, n2, 3)
+l1 = Link("n1", "n2", 1)
+l2 = Link("n2", "n3", 2)
+l3 = Link("n2", "n4", 3)
 t.path.spf_links(n1,n2)
