df1a9a189e3170d543583546715f36e124d028dd
bau047 <bau047@csiro.au>
2017-12-18 09:50:20 +1100

    cmd based cli prototype

--- bflow_cli.py
+++ bflow_cli.py
@@ -15,12 +15,8 @@ class Bflow(cmd.Cmd):
 
     def do_get(self, *args):
         if len(args) > 0:
-            query = pb.GenericQuery(function='ConnectionInfo')
-            responses = querier.send(query)
-            for message in responses:
-                query_response = pb.GenericResponse()
-                query_response.ParseFromString(message)
-                print query_response.data
+            i = Input(args)
+            i.route()
         return
 
     def do_EOF(self, line):
@@ -30,6 +26,51 @@ class Bflow(cmd.Cmd):
         querier.disconnect()
         exit()
 
+    def emptyline(self):
+        return
+
+
+class Input:
+    def __init__(self, args):
+        self.passed_args = args[0].split(" ")
+        self.dispatcher = {
+            'ConnectionInfo': self.connection_info,
+            'GetMacTable': self.get_mac_table,
+        }
+
+    def route(self):
+        command = self.passed_args[0]
+        print command
+        self.passed_args.pop(0)
+        self.dispatcher[command]()
+
+    def connection_info(self):
+        query = pb.GenericQuery(function='ConnectionInfo')
+        responses = querier.send(query)
+        for message in responses:
+            query_response = pb.GenericResponse()
+            query_response.ParseFromString(message)
+            print query_response.data
+
+    def parse_args(self, required, optional, descriptions):
+        required_length = len(optional)
+        arg_length = len(self.passed_args)
+        if arg_length >= required_length:
+            print "Yep"
+
+
+    def get_mac_table(self):
+        required = [
+            'switch'
+        ]
+        optional = [
+            'type'
+        ]
+        descriptions = [
+            'Switch OpenFlow ID',
+            'Query type [NORMAL|DETAILED]'
+        ]
+        self.parse_args(required, optional, descriptions)
 
 querier = Querier(remote_addr='localhost', remote_port=2222)
 querier.connect()
